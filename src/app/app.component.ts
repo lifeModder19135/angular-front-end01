da557f78caf8ef15d5e9a79e8c686c5d3370647d
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-12 13:58:51 -0400

    08122020 -- app.component ts, html -- moved h1 and h2 elements from the html file to the ts file via the @Component 'template:' argument; app.component.ts lines 5 & 6 -- commented out the other @Component args;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
+  template: '<h1>{{title}}</h1><h2>project \'{{project}}\' is selected</h2>'
 })
 export class AppComponent {
   title = 'My Projects';
