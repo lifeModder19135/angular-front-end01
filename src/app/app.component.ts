8bf139f74180e33e474c43fad79ef2cbc5f152be
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-12 15:01:20 -0400

    08122020 -- '''.ts -- Updated value of  template for @Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class Project  {
   selector: 'app-root',
   //templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
-  template: '<h1>{{title}}</h1><h2>project \'{{project}}\' is selected</h2>'
+  template: '<h1>{{title}}</h1><h2>project: \'{{project.name}}\' is selected</h2>'
 })
 export class AppComponent {
   title = 'My Projects';
