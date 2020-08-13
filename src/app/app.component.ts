8e405310a1db927f2d94380dc336c933ed69c7f7
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-13 14:39:56 -0400

    08132020 -- app.component.ts -- Fix: Corrected the 'id' attributes for all objects in the PROJECTS array; for each object, value was changed from 100 to appropriate id value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,17 @@ export class Project  {
 const PROJECTS: Project[] = [
   { id: 1002, name: 'Color Flipper', root: '\\CODING PROJECTS\\Color Flipper', desc:'A CSS \'color flipper\' web app which changes its own background color based on the value of its text input. Built as a CSS-based expiriment.'},
   { id: 1003, name: 'Data Access Layer With Thread Pool', root: '\\CODING PROJECTS\\DAL-With-Thread-Pool', desc: 'An asynchronous Spring Boot Back end DAO setup that accesses h2 datadbase via up to 6 concurrently running threads.'},
-  { id: 100, name: 'Fetch Sandbox', root: '\\CODING PROJECTS\\Fetch Sandbox', desc: 'A Sandbox for expirimenting with the javascript Fetch API.'},
-  { id: 100, name: 'MEAN stack Authorization App', root: '\\CODING PROJECTS\\MEAN-Auth-App', desc: 'MEAN stack app template that I built as a base for all my future apps of the type. Includes built in authorization and authentication...'},
-  { id: 100, name: 'ng-front-end01', root: '\\CODING PROJECTS\\ng-front-end01', desc: 'This project!!!'},
-  { id: 100, name: 'Roblox Studio Worlds', root: '\\CODING PROJECTS\\Roblox Studio Worlds', desc: 'Just a generic workspace for Cadey\'s Roblox Studio Worlds and custom scripts.'},
-  { id: 100, name: 'Snippets etc', root: '\\CODING PROJECTS\\Snippets etc', desc: 'Workspace for random scripts, files, and Snippets that need to be kept with my code but do not belong anywhere else.'},
-  { id: 100, name: 'STS_Workspace', root: '\\CODING PROJECTS\\STS_Workspace', desc: 'Workspace for all my Spring Projects built using Spring Tool Suite.'},
-  { id: 100, name: 'Data Access Layer With Thread Pool', root: '\\CODING PROJECTS\\STS_Workspace\\DAL-With-Thread-Pool', desc: 'Copy of project #1003'},
-  { id: 100, name: 'Springboot Layered API Template', root: '\\CODING PROJECTS\\STS_Workspace\\Springboot_Layered_API_Template', desc: 'Template for a simple Spring Data faccess layer.'},
-  { id: 100, name: 'VS_Code_Debugging_Tutorial', root: '\\CODING PROJECTS\\VS_Code_Debugging_Tutorial', desc: 'Sandbox for expirimenting with Code\'s debugging features for JS and TS/ Angular.'},
-  { id: 100, name: 'VS-Code-Spring-Extension-Demo', root: '\\CODING PROJECTS\\VS-Code-Spring-Extension-Demo', desc: 'Just made this to see how easy / feasible Spring development is via VS Code.'},
-  { id: 100, name: 'Workout Assistant 2 (For-Share version)', root: '\\CODING PROJECTS\\WorkoutAssistant2 - ForShare', desc: '\'safe to share\' version of my most recent Android app \'Dumbbell Workout Assistant\'. Images and other various resources have been removed to prevent piracy.'},
+  { id: 1004, name: 'Fetch Sandbox', root: '\\CODING PROJECTS\\Fetch Sandbox', desc: 'A Sandbox for expirimenting with the javascript Fetch API.'},
+  { id: 1005, name: 'MEAN stack Authorization App', root: '\\CODING PROJECTS\\MEAN-Auth-App', desc: 'MEAN stack app template that I built as a base for all my future apps of the type. Includes built in authorization and authentication...'},
+  { id: 1006, name: 'ng-front-end01', root: '\\CODING PROJECTS\\ng-front-end01', desc: 'This project!!!'},
+  { id: 1007, name: 'Roblox Studio Worlds', root: '\\CODING PROJECTS\\Roblox Studio Worlds', desc: 'Just a generic workspace for Cadey\'s Roblox Studio Worlds and custom scripts.'},
+  { id: 1008, name: 'Snippets etc', root: '\\CODING PROJECTS\\Snippets etc', desc: 'Workspace for random scripts, files, and Snippets that need to be kept with my code but do not belong anywhere else.'},
+  { id: 1009, name: 'STS_Workspace', root: '\\CODING PROJECTS\\STS_Workspace', desc: 'Workspace for all my Spring Projects built using Spring Tool Suite.'},
+  { id: 1010, name: 'Data Access Layer With Thread Pool', root: '\\CODING PROJECTS\\STS_Workspace\\DAL-With-Thread-Pool', desc: 'Copy of project #1003'},
+  { id: 1011, name: 'Springboot Layered API Template', root: '\\CODING PROJECTS\\STS_Workspace\\Springboot_Layered_API_Template', desc: 'Template for a simple Spring Data faccess layer.'},
+  { id: 1012, name: 'VS_Code_Debugging_Tutorial', root: '\\CODING PROJECTS\\VS_Code_Debugging_Tutorial', desc: 'Sandbox for expirimenting with Code\'s debugging features for JS and TS/ Angular.'},
+  { id: 1013, name: 'VS-Code-Spring-Extension-Demo', root: '\\CODING PROJECTS\\VS-Code-Spring-Extension-Demo', desc: 'Just made this to see how easy / feasible Spring development is via VS Code.'},
+  { id: 1014, name: 'Workout Assistant 2 (For-Share version)', root: '\\CODING PROJECTS\\WorkoutAssistant2 - ForShare', desc: '\'safe to share\' version of my most recent Android app \'Dumbbell Workout Assistant\'. Images and other various resources have been removed to prevent piracy.'},
 ];
   
 @Component({
@@ -30,6 +30,12 @@ const PROJECTS: Project[] = [
   template: 
             `
             <h1>{{title}}</h1>
+            <h2>Projects List</h2>
+            <ul class="projects">
+              <li *ngFor= "let project of projects">
+                <span class="badges">{{project.id}}</span> {{project.name}}
+              </li>
+            </ul>
             <h2>project: \'{{project.name}}\' is selected</h2>
             <div><label>Project ID: </label>{{project.id}}</div>
             <div>
@@ -48,5 +54,5 @@ export class AppComponent {
     root: '\\CODING PROJECTS\\MyProject',
     desc: 'This is a simple sample project.'
   };
-  Projects = PROJECTS;
+  projects = PROJECTS;
 } 
