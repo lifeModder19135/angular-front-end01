1a4b00dd90df212de2c89c86422c7d264b890741
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-13 09:51:24 -0400

    08122020 -- app.component.ts -- see last line of previous commit;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,15 +15,20 @@ export class Project  {
             <h1>{{title}}</h1>
             <h2>project: \'{{project.name}}\' is selected</h2>
             <div><label>Project ID: </label>{{project.id}}</div>
-            <div><label>Project Name: </label>{{project.name}}</div>
+            <div>
+              <label>Project Name: </label>
+              <input [(ngModel)]="project.name" placeholder="name">
+            </div>
             <div><label>Root Folder: </label>{{project.root}}</div>
             <div><label>Project Description: </label>{{project.desc}}</div>
             `
 })
 export class AppComponent {
-  title = 'My Projects';
+  title = 'Nate\'s Projects';
   project: Project = {
     id: 1001,
-    name: 'MyProject'
+    name: 'MyProject',
+    root: '\CODING PROJECTS\MyProject',
+    desc: 'This is a simple sample project.'
   };
-}
+} 
